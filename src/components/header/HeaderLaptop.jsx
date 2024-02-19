e954cb3cb2e1c2692cf329be0ca555b5793c10c0
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-02-19 13:43:37 +0200

    fix(Header comp): after deleting FavoritesToServer from HeaderLaptop

--- src/components/header/HeaderLaptop.jsx
+++ src/components/header/HeaderLaptop.jsx
@@ -1,7 +1,5 @@
-// /* eslint-disable react/button-has-type */
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import axios from "axios";
 import IconEnter from "./icons/enter/IconEnter";
 import IconOut from "./icons/enter/IconOut";
 import Logo from "./Logo";
@@ -9,32 +7,14 @@ import { logOut } from "../../redux/actions/loggedInActions";
 import Button from "../button/Button";
 import Navigation from "./Navigation";
 import { resetCart, resetFavorites } from "../../redux/actions/cartActions";
-// import { REGISTRATION_URL } from "../../endpoints/endpoints";
 import styles from "./Header.module.scss";
 
 function HeaderLaptop() {
   const isLoggedInFromRedux = useSelector((state) => state.auth.isLoggedIn);
   const dispatch = useDispatch();
 
-  // const updateFavoritesToServer = async (newFavorites) => {
-  //   try {
-  //     const response = await axios.put(REGISTRATION_URL, {
-  //       favorites: newFavorites,
-  //     });
-  //     return response.data.favorites;
-  //   } catch (error) {
-  //     console.error("Помилка при оновленні улюблених товарів:", error);
-  //     return null;
-  //   }
-  // };
-
   const doLogOut = async () => {
     try {
-      // const currentFavorites =
-      //   JSON.parse(localStorage.getItem("Favorites")) || [];
-      // if (currentFavorites.length > 0) {
-      //   await updateFavoritesToServer(currentFavorites);
-      // }
       localStorage.removeItem("userLogin");
       localStorage.removeItem("isAdmin");
       localStorage.removeItem("CountCartProducts");
